96c6d17be7d0d13cf396d5b40ee026cdd49864c5
Salman Hossain <salman.hossain@greenhouse.io>
2016-01-15 10:32:03 -0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Category.create[
+
+Category.create [
 	{
 		name: "Quality Assurance"
 	},
