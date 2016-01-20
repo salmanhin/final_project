b26febbe4106752af46f471d1231d09a02f26039
Salman Hossain <salman.hossain@greenhouse.io>
2016-01-19 20:45:17 -0500

    updated Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,11 +4,11 @@ class Contact < MailForm::Base
   attribute :message,	:validate => true
   attribute :nickname,  :captcha  => true
 
-def headers
-    {
-      :subject => "Contact Form",
-      :to => "salmanhin@gmail.com,
-      :from => %("#{name}" <#{email}>)
-    }
-  end
+    def headers
+        {
+          :subject => "Contact Form",
+          :to => "salmanhin@gmail.com,
+          :from => %("#{name}" <#{email}>)
+        }
+    end
 end
